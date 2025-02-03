57739dac46727c8887bc86de73dae829597c1aee
FlozyTess <cecilrency23@gmail.com>
2025-02-03 06:25:25 +0300

    completed

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,15 @@
 import React from "react";
+import Links from "./Links"; // Import Links component
 
-function About() {
+function About({ bio, github, linkedin }) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {/* Conditionally render the <p> tag only if bio is provided */}
+      {bio && bio.trim() !== "" && <p>{bio}</p>}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      {/* Pass the props correctly to the Links component */}
+      <Links github={github} linkedin={linkedin} />
     </div>
   );
 }
